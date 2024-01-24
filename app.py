956f9d2bb71992e8cc008313da343778c1f0d417
prasanthi-batchu <prasanthi_batchu@arawinz.com>
2024-01-24 15:33:17 +0530

    remove

--- app.py
+++ app.py
@@ -8,17 +8,6 @@ def hello_world():
    return 'Hello World..'
 
 
-@app.route('/test')
-def test():
-   return 'Testttttttt..'
-
-@app.route('/new_route')
-def new_route():
-   return 'new route...'
-
-@app.route('/test_user')
-def test_user():
-   return 'Hello test user...'
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
